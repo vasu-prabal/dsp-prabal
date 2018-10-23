31820b84b38b2046ae5c1f129a291942e0cdeca7
IN\PAeruva <paeruva2@iis-consulting.com>
2018-10-23 12:03:28 +0530

    moved to component

--- src/app/create-project/create-project.component.ts
+++ src/app/create-project/create-project.component.ts
@@ -1,12 +1,53 @@
-import { Component, OnInit } from "@angular/core";
-
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { Project } from "../home/interfaces";
+declare var jQuery: any;
 @Component({
   selector: "app-create-project",
   templateUrl: "./create-project.component.html",
   styleUrls: ["./create-project.component.css"]
 })
 export class CreateProjectComponent implements OnInit {
+  @ViewChild("createProject")
+  myModal: ElementRef;
+  newProject: Project = {
+    id: undefined,
+    name: "",
+    area: "",
+    laboratory: "",
+    modified: "",
+    owner: ""
+  };
+  dropZone: HTMLElement;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.dropZone = document.getElementById("dropZone");
+    this.dropZone = document.getElementById("dropZone");
+    this.dropZone.ondrop = function(e) {
+      e.preventDefault();
+      console.log(e.dataTransfer.files);
+      jQuery(e.target).removeClass("drop");
+    };
+
+    this.dropZone.ondragover = function(e) {
+      jQuery(e.target).addClass("drop");
+      return false;
+    };
+
+    this.dropZone.ondragleave = function(e) {
+      jQuery(e.target).removeClass("drop");
+      return false;
+    };
+  }
+
+  openCreateProjectDialog() {
+    this.newProject = {};
+    jQuery(this.myModal.nativeElement)
+      .modal({ backdrop: "static", keyboard: false })
+      .modal("show");
+  }
+
+  addNewProject() {
+    console.log(this.newProject);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { ViewChild } from "@angular/core";
-import { ElementRef } from "@angular/core";
 import { HomeService } from "./home.service";
 import { Project } from "./interfaces";
 declare var jQuery: any;
@@ -11,8 +9,6 @@ declare var jQuery: any;
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
-  @ViewChild("createProject")
-  myModal: ElementRef;
   mailsList: Project[];
   newProject: Project = {
     id: undefined,
@@ -22,41 +18,13 @@ export class HomeComponent implements OnInit {
     modified: "",
     owner: ""
   };
-  dropZone = document.getElementById("dropZone");
   constructor(public homeService: HomeService) {}
 
   ngOnInit() {
     this.getMailsList();
-    this.dropZone = document.getElementById("dropZone");
-    this.dropZone.ondrop = function(e) {
-      e.preventDefault();
-      console.log(e.dataTransfer.files);
-      jQuery(e.target).removeClass("drop");
-    };
-
-    this.dropZone.ondragover = function(e) {
-      jQuery(e.target).addClass("drop");
-      return false;
-    };
-
-    this.dropZone.ondragleave = function(e) {
-      jQuery(e.target).removeClass("drop");
-      return false;
-    };
   }
 
   getMailsList() {
     this.mailsList = this.homeService.getMailsList();
-  }
-
-  openCreateProjectDialog() {
-    this.newProject = {};
-    jQuery(this.myModal.nativeElement)
-      .modal({ backdrop: "static", keyboard: false })
-      .modal("show");
-  }
-
-  addNewProject() {
-    console.log(this.newProject);
   }
 }
