425352863e45885f3cc586dd228fbfe2d7505bb1
IN\PAeruva <paeruva2@iis-consulting.com>
2018-11-13 19:15:05 +0530

    added default credentials

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,19 +1,17 @@
 import { ILogin } from "./home/home-modal";
-
-// export const API_URL = "http://pdc-development.esacinc.com:8080/workspace/";
 export const API_URL = "http://pdc-development.esacinc.com:8080/workspace/";
 
-// export const LOCAL_API_URL =
-//   "http://chorusproxy.ap-south-1.elasticbeanstalk.com/getData";
-export const LOCAL_API_URL = "http://localhost:4201/getData";
+export const LOCAL_API_URL =
+  "http://chorusproxy.ap-south-1.elasticbeanstalk.com/getData";
+// export const LOCAL_API_URL = "http://localhost:4201/getData";
 export const loginUserDetails: ILogin = {
   j_username: "ratna.thangudu@esacinc.com", // "demo-user",
   j_password: "pwd",
   _spring_security_remember_me: "on"
-};
+}; // Default credentials
 
 export const PROTOCOL_ADDED = "Protocol Added";
 export const PROJECT_ADDED = "Project Added";
 export const STUDY_ADDED = "Study Added";
 export const INSTRUMENT_MODEL_ADDED = "Instrument Added";
-export const IS_LOCAL_API = true;
+export const IS_LOCAL_API = true; // call our local api based or PDC api
