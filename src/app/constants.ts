969eb639e61fc85eb6352fc092654a175609e835
IN\PAeruva <paeruva2@iis-consulting.com>
2018-11-13 10:46:43 +0530

    changed credentials and removed not implemented links

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -7,7 +7,7 @@ export const API_URL = "http://pdc-development.esacinc.com:8080/workspace/";
 //   "http://chorusproxy.ap-south-1.elasticbeanstalk.com/getData";
 export const LOCAL_API_URL = "http://localhost:4201/getData";
 export const loginUserDetails: ILogin = {
-  j_username: "demo-user",
+  j_username: "ratna.thangudu@esacinc.com", // "demo-user",
   j_password: "pwd",
   _spring_security_remember_me: "on"
 };
